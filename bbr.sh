830b27fc5e255f124780939f55add1d8089918a6
yogurt55 <113217572+yogurt55@users.noreply.github.com>
2025-02-08 00:10:35 +0800

    Create bbr.sh

---
+++ bbr.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "net.core.default_qdisc=fq" | sudo tee -a /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=bbr" | sudo tee -a /etc/sysctl.conf
+sudo sysctl -p
+sysctl net.ipv4.tcp_congestion_control
+sysctl net.core.default_qdisc
+lsmod | grep bbr
